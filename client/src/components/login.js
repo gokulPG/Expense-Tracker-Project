292603906a8b79c13ccf5a95bca3b19f87e6538c
gokul <gokulgangadharanp@gmail.com>
2019-07-07 10:38:17 +0530

    added formik and yup for validation

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,61 +1,63 @@
 import React from 'react'
 import axios from 'axios'
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup'
 
-class Login extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            email:'',
-            password:''
+const SignupSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email')
+      .required('Field Required'),
+    password: Yup.string()
+        .min(6,'Too short!')
+        .max(12,'Too long!')
+        .required('Field Required'),
+
+  });
+  
+
+    class Login extends React.Component{
+        handleSubmit = (values) => {
+            axios.post('http://localhost:3005/login', values)
+                .then((response) => {
+                    if(response.data.errors){
+                        alert(response.data.errors)
+                    }else{
+                        const token = response.data.token
+                        localStorage.setItem('userAuthToken', token)
+                        this.props.history.push('/users/account')
+                    }
+                }) 
         }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+        
+        render(){
+            return(
+                <div>
+                    <h1>LOGIN</h1>
+             <Formik
+                    initialValues={{
+                        email: '',
+                        password:'',
+                    }}
+                    validationSchema={SignupSchema}
+                    onSubmit={this.handleSubmit}
+                    >
+                    {({ errors, touched}) => (
+                        <Form>
+                                email:
+                                <Field name="email" type="email" />
+                                {errors.email && touched.email ? <div>{errors.email}</div> : null}<br/>
+                                password:
+                                <Field name="password" type="password" />
+                                {errors.password && touched.password ? <div>{errors.password}</div> : null}<br/>
 
-    handleChange(e){
-        e.persist()
-        this.setState(() => ({
-            [e.target.name] : e.target.value
-        }))
-    }
-
-    handleSubmit(e){
-        e.preventDefault()
-        const formData = {
-            email: this.state.email,
-            password:this.state.password
+                                <button type="submit">Submit</button>
+                        </Form>
+                    )}
+            </Formik>
+                </div>
+            )
         }
-        axios.post('http://localhost:3005/login', formData)
-             .then((response) => {
-                 if(response.data.errors){
-                     alert(response.data.errors)
-                 }else{
-                     const token = response.data.token
-                     localStorage.setItem('userAuthToken',token)
-                     this.props.history.push('/users/account')
-                 }
-             })
-        }
-
-    render(){
-        return(
-            <div>
-                <h3>LOGIN</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                         email
-                         <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />   
-                    </label><br/>
-                    <label>
-                          password
-                          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />  
-                    </label>
-
-                    <input type="submit" value="submit"/>
-                </form>
-            </div>
-        )
     }
-}
 
 export default Login
+
--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,67 +1,71 @@
 import React from 'react'
 import axios from 'axios'
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup'
 
-class Register extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            username:'',
-            email:'',
-            password:''
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+const SignupSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(6, 'Too Short!')
+      .required('Field Required'),
+    email: Yup.string()
+      .email('Invalid email')
+      .required('Field Required'),
+    password: Yup.string()
+        .min(6,'Too short!')
+        .max(12,'Too long!')
+        .required('Field Required'),
+
+  });
+  
+
+    class  Register extends React.Component{ 
+       handleSubmit =(values) =>{
+            axios.post('http://localhost:3005/register', values)
+                .then((response) => {
+                    if(response.data.errors){
+                        alert(response.data.errors)
+                    }else{
+                        this.props.history.push('/users/login')
+                    }
+                }) 
+       }
+       
+        render(){
+            return (
+                <div>
+                     <h1>REGISTER</h1>
+                     <Formik
+                            initialValues={{
+                                username: '',
+                                email: '',
+                                password:'',
+                            }}
+                            validationSchema={SignupSchema}
+                            onSubmit={this.handleSubmit}
+                            >     
+                            {({ errors, touched}) => (
+                                <Form>
+                                        username:
+                                        <Field name="username" placeholder="Enter your username" />
+                                        {errors.username && touched.username ? (
+                                            <div>{errors.username}</div>
+                                        ) : null}<br/>
+                                        email:
+                                        <Field name="email" type="email" placeholder="abc@gmail.com" />
+                                        {errors.email && touched.email ? <div>{errors.email}</div> : null}<br/>
+                                        password:
+                                        <Field name="password" type="password" placeholder="******" />
+                                        {errors.password && touched.password ? <div>{errors.password}</div> : null}<br/>
+        
+                                        <button type="submit">Submit</button>
+                                </Form>
+                            )}
+                    </Formik>
+                </div>       
+            )}
+        
     }
 
-    handleChange(e){
-        e.persist()
-        this.setState(() => ({
-            [e.target.name]: e.target.value
-        }))
-    }
-
-    handleSubmit(e){
-        e.preventDefault()
-        const formData ={
-            username: this.state.username,
-            email:this.state.email,
-            password:this.state.password
-        }
-        axios.post('http://localhost:3005/register', formData)
-            .then((response) => {
-                if(response.data.errors){
-                    alert(response.data.errors)
-                }else{
-                    this.props.history.push('/users/login')
-                }
-            })  
-    }
-
-
-    render(){
-        return(
-            <div>
-                <h3>REGISTER</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        username:
-                        <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
-                    </label><br/>
-                    <label>
-                        email:
-                        <input type="email" name="email" value={this.state.email} onChange={this.handleChange}/>
-                    </label><br/>
-                    <label>
-                        password:
-                        <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-                    </label><br/>
-
-                    <input type="submit" value="submit" />
-                </form>
-            </div>
-        )
-    }
-
-}
 
 export default Register
+
